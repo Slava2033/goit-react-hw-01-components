f9414498bf8d962e93d569556f93a44591d30d1f
Slava2033 <80854817+Slava2033@users.noreply.github.com>
2021-11-06 17:02:56 +0200

    Add files via upload

---
+++ precache-manifest.47f737ca8630ad8f1ac7b9cccacb7122.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c850c83bc1f9b88ad891650d00f3ed",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "2e642a461c1f54e31cf5",
+    "url": "/goit-react-hw-01-components/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "67200e0149b9f737c99a",
+    "url": "/goit-react-hw-01-components/static/css/main.5a6c7665.chunk.css"
+  },
+  {
+    "revision": "2e642a461c1f54e31cf5",
+    "url": "/goit-react-hw-01-components/static/js/2.50e80e66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.50e80e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67200e0149b9f737c99a",
+    "url": "/goit-react-hw-01-components/static/js/main.632c035f.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
